0a689a4bb832cae1189c9582e40739026c2ec368
ChoiYeongHwan <cyh5343@naver.com>
2024-08-25 20:38:04 +0900

    최영환 : Menu, Title Widget 분리, Title 게임 시작, 종료 이벤트 설정 후 동작 확인

---
+++ Source/UnrealRPG/Widget/Title/TitleMainWidgetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidgetBlueprint.h"
+#include "TitleMainWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALRPG_API UTitleMainWidgetBase : public UUserWidgetBlueprint
+{
+	GENERATED_BODY()
+	
+};
