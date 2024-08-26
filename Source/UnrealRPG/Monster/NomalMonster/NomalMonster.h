d2a2d221d65fb455f00bb06fd046f77692abbb25
ChoiYeongHwan <cyh5343@naver.com>
2024-08-26 18:27:22 +0900

    BP_MeleeMonster, NomalMonster 추가

---
+++ Source/UnrealRPG/Monster/NomalMonster/NomalMonster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Monster/MonsterBase.h"
+#include "NomalMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALRPG_API ANomalMonster : public AMonsterBase
+{
+	GENERATED_BODY()
+	
+};
