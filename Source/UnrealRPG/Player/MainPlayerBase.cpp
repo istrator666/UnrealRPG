e9bc66b2e0cf47987ba8a171bd153c1b7d9d6591
김수원 <rla5233@naver.com>
2024-08-28 21:23:46 +0900

    김수원 : 플레이어 Move 추가

---
+++ Source/UnrealRPG/Player/MainPlayerBase.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/MainPlayerBase.h"
+#include "Player/MainPlayerController.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+AMainPlayerBase::AMainPlayerBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+void AMainPlayerBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Input
+	PlayerController = Cast<AMainPlayerController>(GetController());
+	if (PlayerController)
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			SubSystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+	
+}
+
+void AMainPlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Input
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMainPlayerBase::Move);
+	}
+}
+
+void AMainPlayerBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMainPlayerBase::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (PlayerController)
+	{
+		const FRotator YawRotation(0, PlayerController->GetControlRotation().Yaw, 0);
+		const FVector FowardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(FowardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
---
+++ Source/UnrealRPG/Player/MainPlayerBase.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "MainPlayerBase.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class AMainPlayerController;
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class UNREALRPG_API AMainPlayerBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMainPlayerBase();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+protected:
+	void Move(const FInputActionValue& Instance);
+
+private:
+	// Camera
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArmComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* CameraComponent = nullptr;
+
+private:
+	// Input
+	UPROPERTY()
+	AMainPlayerController* PlayerController = nullptr;
+	
+	UPROPERTY(EditAnywhere, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction = nullptr;
+
+
+
+};
